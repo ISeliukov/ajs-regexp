8be35254d85a911e1e3405e9327604794ff1312d
Garysi <garysi@rambler.ru>
2023-06-13 15:14:01 +0300

    add test

--- src/js/__test__/test.js
+++ src/js/__test__/test.js
@@ -1,7 +1,23 @@
-import {Validator} from '../basic.js';
+import Validator from '../basic.js';
 
 
-test("validator test", () => {
-    let result = new Validator('qwerty');
-    expect(result).toBe();
+test('test username ', () => {
+  let result = Validator.validateUsername(12345);
+  expect(result).toEqual(false);
+
+  result = Validator.validateUsername('gary');
+  expect(result).toEqual(true);
+
+  result = Validator.validateUsername('_gary');
+  expect(result).toEqual(false);
+
+  result = Validator.validateUsername('gary12345');
+  expect(result).toEqual(false);
+
+  result = Validator.validateUsername('tik-tok777q');
+  expect(result).toEqual(true);
+
+  result = Validator.validateUsername('tik_tok777w');
+  expect(result).toEqual(true);
+
 });
--- src/js/basic.js
+++ src/js/basic.js
@@ -1,11 +1,13 @@
-
-
-export class Validator {
-  constructor(name) {
+export default class Validator {
+  static validateUsername(userName) {
+    if (typeof userName !== 'string') {
+      return false;
+    }
+    const regexps = [
+      /[a-zA-Z0-9-_]*$/,
+      /^[^\d-_].*[^\d-_]$/,
+      /\d{4,}/,
+    ];
+    return regexps[0].test(userName) && regexps[1].test(userName) && !regexps[2].test(userName);
   }
-
-  levevalidateUsernamelUp(name) {
-
-  }
-
 }
